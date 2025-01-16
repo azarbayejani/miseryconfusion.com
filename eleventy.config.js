d8706e2421e7fcdf731f58d0596d53df0165e77c
Bobby Azarbayejani <bobby@azarbayejani.com>
2025-01-15 23:23:40 -0800

    Update index to use last commit date

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,25 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
+import childProcess from "child_process";
+
+import { DateTime } from "luxon";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("_redirects");
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["png", "svg"],
+  });
+
+  eleventyConfig.addShortcode("lastCommitDate", function () {
+    const lastUpdatedFromGit = childProcess
+      .execSync(`git log -1 --format=%cd --date=short`)
+      .toString()
+      .trim();
+    const formattedDate = DateTime.fromISO(lastUpdatedFromGit).toLocaleString(
+      DateTime.DATE_FULL
+    );
+    return formattedDate;
   });
 }
