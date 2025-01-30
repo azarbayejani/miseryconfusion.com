10a320333309863191c611e2aac24bface5f98b8
Bobby Azarbayejani <bobby@azarbayejani.com>
2025-01-29 18:52:17 -0800

    Add blog collection

---
+++ blog/blog.11tydata.js
@@ -0,0 +1,26 @@
+import slugify from "@sindresorhus/slugify";
+import { DateTime } from "luxon";
+
+export default function (configData) {
+  return {
+    layout: "main",
+    permalink: function ({ title, page }) {
+      if (!title) {
+        // don't do anything special
+        return;
+      }
+
+      // setting date to false will give the option to disable the date for some blog posts
+      if (page.date === false) {
+        return `/blog/${slugify(title)}/`;
+      }
+
+      const parsedDate = DateTime.fromJSDate(page.date, {
+        // see https://www.11ty.dev/docs/dates/#dates-off-by-one-day
+        zone: "utc",
+      });
+      const formattedDate = parsedDate.toISO().split("T")[0].replace(/-/g, "/");
+      return `/blog/${formattedDate}/${slugify(title)}/`;
+    },
+  };
+}
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,20 +1,31 @@
+import markdownItFootnote from "markdown-it-footnote";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { IdAttributePlugin } from "@11ty/eleventy";
 
 import fs from "fs";
-
 import childProcess from "child_process";
-
 import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
+  // Copy static files
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("js/imageMapResizer.min.js");
   eleventyConfig.addPassthroughCopy("_redirects");
 
+  // Add ID attributes to HTML elements
+  eleventyConfig.addPlugin(IdAttributePlugin);
+
+  // Image transforms
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["png", "svg"],
   });
 
+  // Markdown
+  eleventyConfig.amendLibrary("md", (mdLibrary) =>
+    mdLibrary.use(markdownItFootnote)
+  );
+
+  // Add a shortcode to get the last commit date in DATE_FULL format
   eleventyConfig.addShortcode("lastCommitDate", function () {
     const lastUpdatedFromGit = childProcess
       .execSync(`git log -1 --format=%cd --date=short`)
@@ -26,6 +37,22 @@ export default function (eleventyConfig) {
     return formattedDate;
   });
 
+  // Adds a shortcode to get the current page's created date
+  eleventyConfig.addShortcode("created", function () {
+    return this.page?.inputPath
+      ? fs.statSync(this.page.inputPath).birthtime
+      : undefined;
+  });
+
+  // Adds a shortcode to get the current page's modified date
+  eleventyConfig.addShortcode("modified", function () {
+    return this.page?.inputPath
+      ? fs.statSync(this.page.inputPath).mtime
+      : undefined;
+  });
+
+  // Add a ?v=timestamp to the end of any url to bust the cache
+  // use like {{ "/style.css" | bust }}
   eleventyConfig.addFilter("bust", (url) => {
     const [urlPart, paramPart] = url.split("?");
     const params = new URLSearchParams(paramPart || "");
@@ -40,5 +67,11 @@ export default function (eleventyConfig) {
     } catch (error) {}
 
     return `${urlPart}?${params}`;
+  });
+
+  // Formats a date object as e.g. October 17, 2022
+  // use like {{ post.date | fullDate }}
+  eleventyConfig.addFilter("fullDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
 }
